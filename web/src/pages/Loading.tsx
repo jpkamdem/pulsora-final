5b02771328574381a0b9c519b3c47abae4352961
jpkam <jpkamdem@proton.me>
2025-01-06 17:44:21 +0100

    test

--- web/src/pages/Loading.tsx
+++ web/src/pages/Loading.tsx
@@ -3,22 +3,30 @@ import animatedPulsora from "../assets/logopulsora.mp4";
 
 export default function VideoWithButton() {
   const navigate = useNavigate();
-  
 
   const handleEnter = () => {
-    setTimeout(() => { navigate("/home");}, 0);
+    setTimeout(() => {
+      navigate("/home");
+    }, 0);
   };
-  
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <video autoPlay loop muted className="w-3/4 max-w-lg rounded-lg shadow-lg">
-        <source src={animatedPulsora} type="video/mp4"/>
+      <video
+        autoPlay
+        loop
+        muted
+        className="w-3/4 max-w-lg rounded-lg shadow-lg"
+      >
+        <source src={animatedPulsora} type="video/mp4" />
         Votre navigateur ne supporte pas la lecture vidéo.
       </video>
 
-      <button onClick={handleEnter} className="mt-8 px-6 py-3 text-white bg-blue-900 rounded-lg shadow hover:bg-blue-800 transition">
-        Entrerrr
+      <button
+        onClick={handleEnter}
+        className="mt-8 px-6 py-3 text-white bg-blue-900 rounded-lg shadow hover:bg-blue-800 transition"
+      >
+        Entrer
       </button>
     </div>
   );
