3c2ce972913b725ba190e8b64a9ea506dc309087
jpkam <jpkamdem@proton.me>
2025-01-08 21:35:19 +0100

    changement articles

--- web/src/pages/Articles.tsx
+++ web/src/pages/Articles.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import Vide from "../components/Vide";
 
 type Role = "USER" | "ADMIN";
@@ -21,6 +20,9 @@ interface ArticleInterface {
 
 export default function Articles() {
   const [usersDatas, setUsersDatas] = useState<UserInterface[] | null>(null);
+  const [articlesDatas, setarticlesDatas] = useState<ArticleInterface[] | null>(
+    null
+  );
 
   useEffect(() => {
     const fetchUsersDatas = async () => {
@@ -39,56 +41,56 @@ export default function Articles() {
       }
     };
 
+    const fetchArticleDatas = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/articles");
+        if (!res.ok) {
+          throw new Error(
+            `Erreur dans la récupération des données des matchs : ${res.status}`
+          );
+        }
+
+        const datas = await res.json();
+        setarticlesDatas(datas.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
     fetchUsersDatas();
+    fetchArticleDatas();
   }, []);
   return (
     <>
-      {usersDatas &&
-        usersDatas.map((user) => (
-          <>
-            {user.articles.length > 0 ? (
-              user.articles.map((article) => (
-                <>
-                  <div className="flex justify-center mt-8 mb-8">
-                    <div className=" text-center relative  bg-white border-2 border-gray-100 rounded-t-xl  w-3/6 py-3  ">
-                      <div
-                        key={user.id}
-                        className="absolute  left-10 bg-green-400 px-2  py-0.5  rounded-md"
-                      >
-                        • {user.username}
-                      </div>
-                      <div className="text-gray-400 font-medium ">
-                        {article.title}
-                      </div>
-                    </div>
+      {articlesDatas && articlesDatas.length > 0 ? (
+        usersDatas &&
+        usersDatas.map((user) =>
+          user.articles.map((article) => (
+            <>
+              <div className="flex justify-center mt-8 mb-8">
+                <div className=" text-center relative  bg-white border-2 border-gray-100 rounded-t-xl  w-3/6 py-3  ">
+                  <div
+                    key={user.id}
+                    className="absolute  left-10 bg-green-400 px-2  py-0.5  rounded-md"
+                  >
+                    • {user.username}
                   </div>
-                  <div className="flex justify-center  ">
-                    <div className="flex justify-between  w-3/6   border-gray-100 border-l-2 border-r-2 border-b-2 rounded-b-xl drop-shadow-sm p-4 pl-8">
-                      {article.body}
-                    </div>
+                  <div className="text-gray-400 font-medium ">
+                    {article.title}
                   </div>
-                </>
-              ))
-            ) : (
-              <Vide title="articles" />
-            )}
-          </>
-        ))}
+                </div>
+              </div>
+              <div className="flex justify-center  ">
+                <div className="flex justify-between  w-3/6   border-gray-100 border-l-2 border-r-2 border-b-2 rounded-b-xl drop-shadow-sm p-4 pl-8">
+                  {article.body}
+                </div>
+              </div>
+            </>
+          ))
+        )
+      ) : (
+        <Vide title="articles" />
+      )}
     </>
-  );
-  const navigate = useNavigate();
-
-  const handleEnter = () => {
-    setTimeout(() => {
-      navigate("/nutrition");
-    }, 0);
-  };
-  return (
-    <button
-      onClick={handleEnter}
-      className="mt-8 px-6 py-3 text-white bg-blue-900 rounded-lg shadow hover:bg-blue-800 transition"
-    >
-      NUTRITION
-    </button>
   );
 }
