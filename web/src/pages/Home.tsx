638dc36658f76f60ad935fa5e5d54f91e29ef045
jpkam <jpkamdem@proton.me>
2025-01-08 18:17:58 +0100

    changement légers pr prod

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import playerHome from "../assets/playerhome.png";
 import img2 from "../assets/img2.jpg";
 
-import { Link } from "react-router-dom";
-
 export default function Home() {
   return (
     <>
@@ -12,7 +10,7 @@ export default function Home() {
           <div className="text-center absolute bottom-0 lg:relative">
             <h1 className="text-4xl font-bold lg:text-8xl">PULSORA</h1>
             <h2 className="text-3xl font-semibold lg:text-3xl text-blue-900">
-            L'univers du football, à portée de main !
+              L'univers du football, à portée de main !
             </h2>
           </div>
         </div>
@@ -23,22 +21,25 @@ export default function Home() {
           </button>
         </div>
 
-
-
         <div className="bg-white text-blue-900 p-10 lg:p-20 flex flex-col lg:flex-row gap-10 items-center ">
           <div className="flex-1 text-left">
             <h2 className="text-3xl font-bold lg:text-5xl mb-4 ">
-            Gestion des équipes sportives            </h2>
+              Gestion des équipes sportives{" "}
+            </h2>
             <p className="text-lg lg:text-xl mb-6">
-            Pulsora facilite la gestion des équipes sportives, l’organisation des entraînements 
-            et la communication entre entraîneurs, joueurs et responsables, tout en permettant aux 
-            fans de suivre l’actualité des équipes et des joueurs
+              Pulsora facilite la gestion des équipes sportives, l’organisation
+              des entraînements et la communication entre entraîneurs, joueurs
+              et responsables, tout en permettant aux fans de suivre l’actualité
+              des équipes et des joueurs
             </p>
-
           </div>
 
           <div className="flex-1 relative">
-            <img className="w-5/6 md:w-4/6 rounded-lg shadow-lg ml-20"  src={img2} alt="Player Home"/>
+            <img
+              className="w-5/6 md:w-4/6 rounded-lg shadow-lg ml-20"
+              src={img2}
+              alt="Player Home"
+            />
           </div>
         </div>
       </div>
