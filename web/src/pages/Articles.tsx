c49f5396e30e1726a95893e5761a8b2ab7b13a24
jpkam <jpkamdem@proton.me>
2025-01-21 22:12:13 +0100

    modification article new api

--- web/src/pages/Articles.tsx
+++ web/src/pages/Articles.tsx
@@ -1,95 +1,41 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import Vide from "../components/Vide";
-
-type Role = "USER" | "ADMIN";
-
-interface UserInterface {
-  id: number;
-  username: string;
-  password: string;
-  role: Role;
-  articles: ArticleInterface[];
-}
-
-interface ArticleInterface {
-  id: number;
-  title: string;
-  body: string;
-  authorId: number;
-}
+import { useAllUsersWithPosts } from "../utils/hooks";
 
 export default function Articles() {
-  const [usersDatas, setUsersDatas] = useState<UserInterface[] | null>(null);
-  const [articlesDatas, setarticlesDatas] = useState<ArticleInterface[] | null>(
-    null
-  );
-
-  useEffect(() => {
-    const fetchUsersDatas = async () => {
-      try {
-        const res = await fetch("http://localhost:3000/users");
-        if (!res.ok) {
-          throw new Error(
-            `Erreur dans la récupération des données des matchs : ${res.status}`
-          );
-        }
-
-        const datas = await res.json();
-        setUsersDatas(datas.data);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    const fetchArticleDatas = async () => {
-      try {
-        const res = await fetch("http://localhost:3000/articles");
-        if (!res.ok) {
-          throw new Error(
-            `Erreur dans la récupération des données des matchs : ${res.status}`
-          );
-        }
-
-        const datas = await res.json();
-        setarticlesDatas(datas.data);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    fetchUsersDatas();
-    fetchArticleDatas();
-  }, []);
+  const { AllUsersWithPosts, loading } = useAllUsersWithPosts();
   return (
     <>
-      {articlesDatas && articlesDatas.length > 0 ? (
-        usersDatas &&
-        usersDatas.map((user) =>
-          user.articles.map((article) => (
-            <>
-              <div className="flex justify-center mt-8 mb-8">
-                <div className=" text-center relative  bg-white border-2 border-gray-100 rounded-t-xl  w-3/6 py-3  ">
-                  <div
-                    key={user.id}
-                    className="absolute  left-10 bg-green-400 px-2  py-0.5  rounded-md"
-                  >
-                    • {user.username}
-                  </div>
-                  <div className="text-gray-400 font-medium ">
-                    {article.title}
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-center  ">
-                <div className="flex justify-between  w-3/6   border-gray-100 border-l-2 border-r-2 border-b-2 rounded-b-xl drop-shadow-sm p-4 pl-8">
-                  {article.body}
-                </div>
-              </div>
-            </>
-          ))
-        )
+      {loading ? (
+        <p>Chargement</p>
       ) : (
-        <Vide title="articles" />
+        <>
+          {AllUsersWithPosts && AllUsersWithPosts.length > 0 ? (
+            <>
+              {AllUsersWithPosts.map((item) => (
+                <React.Fragment key={item.post_id}>
+                  <div className="flex justify-center mt-8 mb-8">
+                    <div className=" text-center relative bg-white border-2 border-gray-100 rounded-t-xl w-3/6 py-3  ">
+                      <div className="absolute left-10 bg-green-400 px-2 py-0.5 rounded-md">
+                        <p>• {item.username}</p>
+                      </div>
+                      <div className="text-gray-400 font-medium">
+                        <p>{item.title}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex justify-center">
+                    <div className="flex justify-between w-3/6 border-gray-100 border-l-2 border-r-2 border-b-2 rounded-b-xl drop-shadow-sm p-4 pl-8">
+                      <p>{item.content}</p>
+                    </div>
+                  </div>
+                </React.Fragment>
+              ))}
+            </>
+          ) : (
+            <Vide title="articles" />
+          )}
+        </>
       )}
     </>
   );
